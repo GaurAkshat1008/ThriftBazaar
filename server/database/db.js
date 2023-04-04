e1370f1d999ef1f6e1972ed84ad9012c5551fcf6
Akshat <akshatgauriiitsurat@gmail.com>
2023-04-04 21:15:02 +0530

    made changes

--- server/database/db.js
+++ server/database/db.js
@@ -1,8 +1,7 @@
 import mongoose from "mongoose";
 const Connection = async () => {
   try {
-    const url =
-      "mongodb+srv://sem6:yE08K7gGaEHD449s@thriftbazaar.asndmtr.mongodb.net/?retryWrites=true&w=majority";
+    const url = process.env.MONGODB_URL;
     mongoose.connect(url, { useUnifiedTopology: true, useNewUrlParser: true });
     console.log("database connected successfully");
   } catch (error) {
